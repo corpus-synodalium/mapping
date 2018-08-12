0c4bdb639af5058dbf2deac630d1ab4f7ec87937
thawsitt <thawsitt@cs.stanford.edu>
2018-08-11 23:18:44 -0400

    Add simple working example

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './App.css';
+import Map from './Map';
+
+class App extends Component {
+  render() {
+    return <Map />;
+  }
+}
+
+export default App;
