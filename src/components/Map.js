a2cf031a171d57e2dafd10a4c7f1c8a4e8abf2f9
thawsitt <thawsitt@cs.stanford.edu>
2018-08-12 00:06:24 -0400

    Load geojson file with pretty colors :sparkles:

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Map, TileLayer, GeoJSON } from 'react-leaflet';
 import mapConfig from '../assets/map_config';
+import geojson from '../assets/great_britain.json';
 import './Map.css';
 
 export default class SimpleExample extends Component {
@@ -8,6 +9,24 @@ export default class SimpleExample extends Component {
     super(props);
     this.state = {
       config: mapConfig,
+    };
+  }
+
+  style(feature) {
+    const colors = ['#f6eff7', '#bdc9e1', '#67a9cf', '#1c9099', '#016c59'];
+
+    const getColor = (shpfid) => {
+      const id = parseInt(shpfid.substring(1, 5), 10);
+      return colors[id % colors.length];
+    };
+
+    return {
+      fillColor: getColor(feature.properties.SHPFID),
+      weight: 2,
+      opacity: 3,
+      color: 'white',
+      dashArray: '3',
+      fillOpacity: 0.5,
     };
   }
 
@@ -22,6 +41,7 @@ export default class SimpleExample extends Component {
           accessToken={config.tileLayer.params.accessToken}
           maxZoom={config.tileLayer.params.maxZoom}
         />
+        <GeoJSON data={geojson} style={this.style} />
       </Map>
     );
   }
