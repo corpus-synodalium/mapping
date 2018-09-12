397432f7a18f54000eaaf1fb990007813abac848
thawsitt <thawsitt@cs.stanford.edu>
2018-09-11 22:42:42 -0400

    Register service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import 'semantic-ui-css/semantic.min.css';
-//import registerServiceWorker from "./registerServiceWorker";
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-//registerServiceWorker();
+registerServiceWorker();
