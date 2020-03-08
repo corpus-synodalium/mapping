eb6c41570a6ed9ed40c94b1e04fb67c8b66d021a
thawsitt <thawsitt@gmail.com>
2020-03-08 15:21:08 -0700

    Use dropdown instead of checkbox for layer selection
    - Provide 3 options:
    1. dioceses only
    2. provinces only
    3. dioceses + provinces

---
+++ src/components/cosyn-constants.js
@@ -0,0 +1,41 @@
+export const COLOR_SCHEMES = [
+  {
+    text: 'Black & White',
+    value: 'bw',
+  },
+  {
+    text: 'Colors #1',
+    value: 'color1',
+  },
+  {
+    text: 'Colors #2',
+    value: 'color2',
+  },
+  {
+    text: 'Colors #3',
+    value: 'color3',
+  },
+  {
+    text: 'Colors #4',
+    value: 'color4',
+  },
+];
+
+export const DIOCESE = 'DIOCESE';
+export const PROVINCE = 'PROVINCE';
+export const DIOCESE_PROVINCE = 'DIOCESE_PROVINCE';
+
+export const LAYER_VIEW_MODES = [
+  {
+    text: 'Dioceses',
+    value: DIOCESE,
+  },
+  {
+    text: 'Provinces',
+    value: PROVINCE,
+  },
+  {
+    text: 'Dioceses + Provinces',
+    value: DIOCESE_PROVINCE,
+  },
+];
