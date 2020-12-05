8f6132031e0374b5e209504ff5f6c447d78b3ba4
thawsitt <thawsitt@cs.stanford.edu>
2020-12-04 16:58:51 -0800

    Unregister service worker due to cache issues

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './App';
-import 'semantic-ui-css/semantic.min.css';
-import registerServiceWorker from './registerServiceWorker';
 import ReactGA from 'react-ga';
+import 'semantic-ui-css/semantic.min.css';
+import App from './App';
+import { unregister } from './registerServiceWorker';
+import './styles/index.css';
 
 ReactGA.initialize('UA-135535706-1');
 ReactGA.pageview('cosyn.app homepage');
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+unregister();
