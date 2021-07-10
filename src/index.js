0d97582310f0bfa99fdd5eebe6fd0a76c171c908
thawsitt <thawsitt@gmail.com>
2021-07-09 19:18:11 -0700

    Update GA id

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import { unregister } from './registerServiceWorker';
 import './styles/index.css';
 
-ReactGA.initialize('UA-135535706-1');
+ReactGA.initialize('UA-201810694-1');
 ReactGA.pageview('cosyn.app homepage');
 ReactDOM.render(<App />, document.getElementById('root'));
 unregister();
